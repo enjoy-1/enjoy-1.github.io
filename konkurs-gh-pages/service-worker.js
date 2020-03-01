922a67415431a0d18857cd393c3a47fbb9e04508
enjoy-1 <59749662+enjoy-1@users.noreply.github.com>
2020-03-01 16:55:30 +0200

    Update service-worker.js

--- konkurs-gh-pages/service-worker.js
+++ konkurs-gh-pages/service-worker.js
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/konkurs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/konkurs-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
